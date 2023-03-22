79b65bb1cc327d2153055aa55d2100b3a52fa6f8
andymuhammadsandika <andymuhammadsandika@gmail.com>
2023-03-23 00:30:57 +0700

    initial commit

---
+++ src/handler.js
@@ -0,0 +1,96 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = (request, h) => {
+  const {
+    name, 
+    year, 
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  const id = nanoid(16);
+  const finished = pageCount === readPage;
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+
+  const userBook = {
+    id, 
+    name, 
+    year, 
+    author, 
+    summary, 
+    publisher, 
+    pageCount, 
+    readPage, 
+    finished, 
+    reading, 
+    insertedAt, 
+    updatedAt,
+  };
+
+  books.push(userBook);
+
+  const isInserted = books.filter((book) => book.id).length > 0;
+
+  // Client tidak melampirkan properti namepada request body.
+  if (name === '') {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+  
+  // Client melampirkan nilai properti readPage yang lebih besar dari nilai properti pageCount.
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+  // Bila buku berhasil dimasukkan
+  if (isInserted) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+  // kesalahan umum.
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
+
+const getAllBooksHandler = () => ({
+  status: 'success',
+  data: {
+    books: books.map((book) => ({
+      id: book.id,
+      name: book.name,
+      publisher: book.publisher,
+    })),
+  },
+});
+
+module.exports = {
+  addBookHandler,
+  getAllBooksHandler,
+};
